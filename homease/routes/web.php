f861c6c4f1929727a97a149069f65790c8ab2c6d
Jemuel Olaybar <jem.olaybar@gmail.com>
2025-02-07 00:26:49 +0800

    navbar and cover photo - done

--- homease/routes/web.php
+++ homease/routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', function () {
+    return view('auth.login'); // Replace with your actual login view
+})->name('login');
+
+Route::get('/register', function () {
+    return view('auth.register'); // Replace with your actual login view
+})->name('register');
